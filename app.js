029cb86b0fb1aee2b592867439de4c7e9b790261
Lemyrik <109064150+Lemyrik@users.noreply.github.com>
2022-09-03 16:32:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['#F5FF54', '#E28888', '#88A3E2', '#A3E288','#B8E288']
+const SQUARES_NUMBER = 810
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+    setColor(square))
+    board.append(square)
+    
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+    
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
